0c8c9b26b6df51ce07235108955f44a9ee56a3e6
SashaNeftin <SashaNeftin@5811ca8c-92f0-11dd-8670-79a04d17b783>
2008-10-05 18:30:44 +0000

    add CRC Calculator.h

---
+++  crc-32/CRC/CRCCalculator.h
@@ -0,0 +1,36 @@
+#ifndef CRCCALCULATOR_H_
+#define CRCCALCULATOR_H_
+
+#include <stdint.h>
+
+namespace nts
+{
+
+class CRCCalculator
+{
+public:
+    //Constructors
+	CRCCalculator();
+
+	virtual ~CRCCalculator();
+
+    virtual const uint32_t CalculateCRC32(const uint8_t* message, const unsigned int len);
+
+    virtual uint8_t* AppendCRC32(uint8_t* where, const unsigned int len);
+
+private:
+    //Forbid copy constructor, assignment operator
+    CRCCalculator(const CRCCalculator& copy);
+    CRCCalculator& operator=(const CRCCalculator& assign);
+
+    //CRC-32 located
+    uint32_t m_crc;
+
+};
+
+}
+
+#endif /*CRCCALCULATOR_H_*/
+
+
+
